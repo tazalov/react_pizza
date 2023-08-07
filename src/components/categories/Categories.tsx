a0e1d80ff9a1cd0ca8085781ada2a0eb34ba8734
tazalov <tazalov9@gmail.com>
2023-08-07 16:09:04 +0300

    Add local state for categories

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type CategoriesPT = {
   // Добавьте свойства пропсов здесь
 };
 
 export function Categories(props: CategoriesPT) {
+  const categories = [
+    {
+      id: 0,
+      name: 'All',
+    },
+    {
+      id: 1,
+      name: 'Meat',
+    },
+    {
+      id: 2,
+      name: 'Vegetarian',
+    },
+    {
+      id: 3,
+      name: 'Grill',
+    },
+    {
+      id: 4,
+      name: 'Sharp',
+    },
+    {
+      id: 5,
+      name: 'Closed',
+    },
+  ];
+
+  const [categoryNum, setCategoryNum] = useState<number>(0);
+
+  const changeCategory = (n: number) => setCategoryNum(n);
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">All</li>
-        <li>Meat</li>
-        <li>Vegetarian</li>
-        <li>Grill</li>
-        <li>Sharp</li>
-        <li>Closed</li>
+        {categories.map((el) => (
+          <li
+            key={el.id}
+            className={categoryNum === el.id ? 'active' : ''}
+            onClick={() => changeCategory(el.id)}>
+            {el.name}
+          </li>
+        ))}
       </ul>
     </div>
   );
