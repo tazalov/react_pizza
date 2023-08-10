f2a5ee461db4b777224e8b9d62a3e060744eb57b
tazalov <tazalov9@gmail.com>
2023-08-10 13:44:50 +0300

    Add adaptive

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -15,7 +15,7 @@ export function Header(props: HeaderPT) {
             <img width="38" src={logo} alt="Pizza logo" />
             <div>
               <h1>React Pizza</h1>
-              <p>the most delicious pizza in the universe</p>
+              <p>The best pizza in the universe</p>
             </div>
           </div>
         </NavLink>
