528eb2275c96ee071ba3a30d8714d8106e4bf90f
tazalov <tazalov9@gmail.com>
2023-08-19 14:05:39 +0300

    Installing lodash, creating debounce for search input

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,32 +1,45 @@
 import s from './Search.module.scss';
 import searchIcon from '../../assets/img/icons8-search.svg';
 import closeIcon from '../../assets/img/icons8-close.svg';
-import { ChangeEvent, useContext } from 'react';
+import { ChangeEvent, useCallback, useContext, useRef, useState } from 'react';
 import { SearchContext } from '../../app/App';
+import debounce from 'lodash.debounce';
 
 type SearchPT = {};
 
 export function Search({}: SearchPT) {
-  const { search, setSearch } = useContext(SearchContext);
+  const [value, setValue] = useState<string>('');
+  const { setSearch } = useContext(SearchContext);
+  const searchInputRef = useRef<HTMLInputElement | null>(null);
+
+  const updateSearchValue = useCallback(
+    debounce((str) => setSearch(str), 300),
+    [],
+  );
 
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.currentTarget.value);
+    setValue(e.currentTarget.value);
+    updateSearchValue(e.currentTarget.value);
   };
   const onClickClearHandler = () => {
+    setValue('');
     setSearch('');
+    searchInputRef.current?.focus();
   };
+
   return (
     <div className={s.root}>
       <div className={s.search}>
         <img src={searchIcon} alt="" />
       </div>
       <input
+        ref={searchInputRef}
         className={s.input}
         placeholder={'Find your pizzas!'}
-        value={search}
+        value={value}
         onChange={onChangeHandler}
       />
-      {search && (
+      {value && (
         <div className={s.clear} onClick={onClickClearHandler}>
           <img src={closeIcon} alt="" />
         </div>
